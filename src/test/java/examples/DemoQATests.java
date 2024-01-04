a8b0d62784941b07f0f905ef721efbe8e482a3d0
Marina Komisarchuk <komisarchukmarina.003@gmail.com>
2024-01-04 22:49:01 +0300

    hw done

---
+++ src/test/java/examples/DemoQATests.java
@@ -0,0 +1,60 @@
+package examples;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DemoQATests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen=true;
+
+    }
+
+    @Test
+    void fillFormTest() {
+        String userName = "Marina";
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue("Marina");
+        $("#lastName").setValue("Komisarchuk");
+        $("#userEmail").setValue("komisarchukmarina.003@gmail.com");
+        $("#gender-radio-2").parent().click();
+//        $("#genterWrapper").$(byText("Female")).click(); альтернатива
+//        $("label[for=gender-radio-2]").click(); альтернатива
+        $("#userNumber").setValue("5432345674");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").selectOption("2003");
+        $(".react-datepicker__day--008").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#currentAddress").setValue("Nezavisimosti 130");
+        $("#hobbies-checkbox-3").parent().click();
+//        $("#uploadPicture").uploadFile(new File("src/test/resources/img/1.png"));
+        $("#uploadPicture").uploadFromClasspath(("img/1.png"));
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Karnal")).click();
+        $("#submit").click();
+
+        $(".modal-dialog").should(appear); // должен появиться
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Komisarchuk"),text(userName),
+                text("komisarchukmarina.003@gmail.com"),text("5432345674"));
+
+    }
+
+}
---
+++ src/test/java/examples/JUnit5Examples.java
@@ -0,0 +1,41 @@
+package examples;
+
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class JUnit5Examples {
+
+    @BeforeAll
+    static void setUp() {
+        System.out.println("___setUp()");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("_____beforeEach()");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("_______firstTest()");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("_______secondTest()");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("_____afterEach()");
+    }
+
+    @AfterAll
+    static void tearDown() {
+        System.out.println("___tearDown()");
+    }
+
+}
